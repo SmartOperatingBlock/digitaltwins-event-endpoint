aefa3a8e20b99d378963d8e201730eeb7a0dd893
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-10 10:28:27 +0100

    feat: listen events coming from event grid

--- adt-event-endpoint-function/AdtEventEndpoint.cs
+++ adt-event-endpoint-function/AdtEventEndpoint.cs
@@ -1,19 +1,25 @@
-// Default URL for triggering event grid function in the local environment.
-// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 using System;
+using Azure.Messaging;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
-using Azure.Messaging.EventGrid;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AdtEventEndpoint
 {
     public static class AdtEventEndpoint
     {
-        [FunctionName("AdtEventEndpoint")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        [FunctionName("broadcast")]
+        public static void Observe(
+            [EventGridTrigger] CloudEvent eventGridEvent,
+            ILogger log)
         {
-            log.LogInformation(eventGridEvent.Data.ToString());
+            JObject message = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+            message.Add("id", eventGridEvent.Subject);
+            message.Add("eventType", eventGridEvent.Type);
+            message.Add("eventDateTime", eventGridEvent.Time.ToString());
+            log.LogInformation($"Reading event: {message.ToString()}");
         }
     }
 }
