bbbac0433f14311e5096736305706e7e58cee3ed
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-10 12:10:55 +0100

    docs: document the functions

--- adt-event-endpoint-function/AdtEventEndpoint.cs
+++ adt-event-endpoint-function/AdtEventEndpoint.cs
@@ -13,6 +13,11 @@ namespace AdtEventEndpoint
 {
     public static class AdtEventEndpoint
     {
+        /// <summary>A HTTP trigger function. It is used by client to be able to connect to SignalR Service.
+        /// It uses the SignalRConnectionInfo input binding
+        /// to generate and return valid connection information.</summary>
+        /// <param name="req">the trigger of the function. Client perform a post request on this function in order to obtain the token.</param>
+        /// <param name="connectionInfo">the connection information returned to the client.</param>
         [FunctionName("negotiate")]
         public static SignalRConnectionInfo GetSignalRInfo(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
@@ -21,23 +26,31 @@ namespace AdtEventEndpoint
             return connectionInfo;
         }
 
+        /// <summary>This Azure function handle the events from the Event Grid topic 
+        /// to which it is subscribed in order to receive Azure Digital Twins events.</summary>
+        /// <param name="eventGridEvent">the trigger of the function. It is an event from the Event Grid. The specification used is CloudEvent v1.</param>
+        /// <param name="signalRConnection">the output binding to the SignalR connection used to send the events.</param>
         [FunctionName("broadcast")]
         public static Task Observe(
             [EventGridTrigger] CloudEvent eventGridEvent,
-            [SignalR(HubName = "dt-event-endpoint-hub", ConnectionStringSetting = "AzureSignalRConnectionString")] IAsyncCollector<SignalRMessage> signalRMessages,
+            [SignalR(HubName = "dt-event-endpoint-hub", ConnectionStringSetting = "AzureSignalRConnectionString")] IAsyncCollector<SignalRMessage> signalRConnection,
             ILogger log)
         {
-            JObject message = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
-            message.Add("id", eventGridEvent.Subject);
-            message.Add("eventType", eventGridEvent.Type);
-            message.Add("eventDateTime", eventGridEvent.Time.ToString());
-            log.LogInformation($"New event:\n {message.ToString()}");
+            // Obtain event data and construct the event object to send via SignalR
+            JObject eventToClients = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+            // Add metadata to the event object
+            eventToClients.Add("id", eventGridEvent.Subject);
+            eventToClients.Add("eventType", eventGridEvent.Type);
+            eventToClients.Add("eventDateTime", eventGridEvent.Time.ToString());
+            
+            log.LogInformation($"New event:\n {eventToClients.ToString()}");
 
-            return signalRMessages.AddAsync(
+            // Send the event via SignalR
+            return signalRConnection.AddAsync(
                 new SignalRMessage
                 {
                     Target = "newMessage",
-                    Arguments = new[] { JsonConvert.SerializeObject(message) }
+                    Arguments = new[] { JsonConvert.SerializeObject(eventToClients) }
                 });
         }
     }
